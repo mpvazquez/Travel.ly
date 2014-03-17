ce5085b39d747f284b203548e57080af1192aa11
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2014-03-17 11:32:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Travelly::Application.config.secret_key_base = 'c91cfa7079310894cbf0006f888cab7020db5e7050ee6a31a6cf31792d1868a19a9f536f2bd2cbb907abe4fd49e678dd7ddaf2bef519de63087166c20763fdd7'
