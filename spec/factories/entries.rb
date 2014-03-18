e8d5deb4f11b666cacb10f1078b9cfa9a6d23d2c
Zack Stayman <z.stayman@gmail.com>
2014-03-18 11:57:46 -0400

    stops show completed

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :entry do
     association :stop, factory: :stop, strategy: :build
-    text
+    text "I have returned"
+    title "I have returned"
   end
 end
--- spec/features/user_can_see journal_entries_related_to_a_stop.rb
+++ spec/features/user_can_see journal_entries_related_to_a_stop.rb
@@ -4,9 +4,15 @@ describe "user can see journal entries related to a stop" do
 
   it "shows all the journal entries related to a stop" do
     @stop = FactoryGirl.create(:stop)
+    @entry = FactoryGirl.create(:entry, stop_id: @stop.id)
+    @not_entry = FactoryGirl.create(:entry, text: "THIS SHOULDN'T SHOW UP")
+
     visit stop_path(@stop)
     expect(page).to have_content(@stop.place.city)
-    expect(page).to have_content(@)
+    expect(page).to have_content(@entry.text)
+    expect(page).to have_content(@entry.title)
+    expect(page).not_to have_content(@not_entry.text)
+
   end
 
 end
