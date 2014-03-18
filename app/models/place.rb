6f5c0fd6832d74f3e4d5f168ab6fb04ef00fff08
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2014-03-18 11:55:39 -0400

    add test for place uniqueness

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,5 +4,6 @@ class Place < ActiveRecord::Base
 	has_many :stops
 
 	validates :city, :state, :country, :latitude, :longitude, :google_id, :photo_url, :description, presence: true
+  validates :google_id, uniqueness: true
 
 end
